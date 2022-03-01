4beb5d125622b5b8a4ffb4bbcd51ec1fb3fed775
shafin009 <ghostboyshafin@example.com>
2022-03-01 23:22:01 +0600

    tanvir

--- script.js
+++ script.js
@@ -14,13 +14,12 @@ const searchPhone = () => {
     const errorMessageDiv = document.getElementById("error");
     errorMessageDiv.textContent = '';
 
+
     //----------------------Fetch Section-------------------
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayResult(data.data.slice(0, 20)))
-
-
 
 };
 
@@ -30,16 +29,16 @@ const displayResult = (phones) => {
     const searchResult = document.getElementById('search-result');
     searchResult.innerHTML = '';
 
-    //-------------------For Each Section-----------------
+    //------------Error Message (Search Button)--------------------
     if (phones.length == 0) {
         const errorMessageDiv = document.getElementById("error");
 
         errorMessageDiv.innerHTML = `
-                <p class="text-center text-light">Your search did not found</p>
+                <p class="text-center fs-1 fw-bold bg-primary bg-opacity-75 rounded-pill text-light mx-5">Sorry !Your Search did not Found.</p>
              
               `;
     }
-
+    //-------------------ForEach Section--------------------
     phones.forEach(phone => {
 
         const div = document.createElement('div');
@@ -52,9 +51,9 @@ const displayResult = (phones) => {
             <div class="card h-100 p-4">
                      <img src="${phone.image}" class="card-img-top" alt="...">
                  <div class="card-body">
-                     <h5 class="card-title">Model-${phone.phone_name}</h5>
-                     <h5 class="card-title">Brand-${phone.brand}</h5>
-                     <button onclick="seeDetails('${phone.slug}')"type="button" class="btn btn-primary btn-sm">Details</button>
+                     <h5 class="card-title"><span class="fw-bold">Model- </span>${phone.phone_name}</h5>
+                     <h5 class="card-title"><span class="fw-bold">Brand- </span>${phone.brand}</h5>
+                     <button onclick="seeDetails('${phone.slug}')"type="button" class="btn btn-success btn-sm">Details</button>
                  </div>
             </div>
         
@@ -69,19 +68,27 @@ const displayResult = (phones) => {
 
 };
 
+//---------------------Phone id Section------------
 const seeDetails = (id) => {
 
     //----------------------2nd Fetch Section-------------
     const url2 = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url2)
         .then(res => res.json())
-        .then(data => information(data.data))
+        .then(data => productDetails(data.data))
 
 
 }
-const information = (details) => {
+const productDetails = (details) => {
 
+    if (details.others == null) {
+        const errorMessageDiv = document.getElementById("error");
 
+        errorMessageDiv.innerHTML = `
+                <p class="text-center fs-1 fw-bold bg-primary bg-opacity-75 rounded-pill text-light mx-5">Sorry !Your Search did not Found.</p>
+             
+              `;
+    }
 
     const mobileDetails = document.getElementById('mobile-details');
     mobileDetails.innerHTML = ' ';
@@ -94,20 +101,20 @@ const information = (details) => {
     <div class="card shadow-lg text-center mx-auto pt-4">
         <img src="${details.image}" class="card-img-top img-fluid w-25 mx-auto" alt="...">
      <div class="card-body">
-            <p class="card-title">Model- ${details.name}</p>
-            <p class="card-title">Brand- ${details.brand}</p>
-            <p class="card-title">Memory- ${details.mainFeatures.memory}</p>
-            <p class="card-title">Chipset- ${details.mainFeatures.chipSet}</p>
-            <p class="card-title">Display Size- ${details.mainFeatures.displaySize}</p>
-            <p class="card-title">Sensors- ${details.mainFeatures?.sensors}</p>
-            <p class="card-title">Release Date- ${details?.releaseDate}</p>
-            <p class="text-center text-success">More Details <br>_________</p>
+            <p class="card-title"><span class="fw-bold">Model- </span>${details.name}</p>
+            <p class="card-title"><span class="fw-bold">Brand- </span>${details.brand}</p>
+            <p class="card-title"><span class="fw-bold">Memory- </span>${details.mainFeatures.memory}</p>
+            <p class="card-title"><span class="fw-bold">Chipset- </span>${details.mainFeatures.chipSet}</p>
+            <p class="card-title"><span class="fw-bold">Display Size- </span>${details.mainFeatures.displaySize}</p>
+            <p class="card-title"><span class="fw-bold">Sensors- </span>${details.mainFeatures?.sensors}</p>
+            <p class="card-title"><span class="fw-bold">Release Date- </span>${details?.releaseDate}</p>
+            <p class="text-center text-success"><span class="fw-bold">More Details</span><br>_________</p>
             
-            <p class="card-title">NFC- ${details.others?.NFC}</p>
-            <p class="card-title">USB- ${details.others?.USB}</p>
-            <p class="card-title">WLAN- ${details.others?.WLAN}</p>
-            <p class="card-title">GPS- ${details.others?.GPS}</p>
-            <p class="card-title">Radio- ${details.others?.Radio}</p>
+            <p class="card-title"><span class="fw-bold">NFC- </span>${details.others?.NFC}</p>
+            <p class="card-title"><span class="fw-bold">USB- </span>${details.others?.USB}</p>
+            <p class="card-title"><span class="fw-bold">WLAN- </span>${details.others?.WLAN}</p>
+            <p class="card-title"><span class="fw-bold">GPS- </span>${details.others?.GPS}</p>
+            <p class="card-title"><span class="fw-bold">Radio- </span>${details.others?.Radio}</p>
             
             
         </div>    
@@ -122,20 +129,20 @@ const information = (details) => {
 };
 
 
-const errorMessage = () => {
-    const searchText = document.getElementById("input").value;
-    const errorMessageDiv = document.getElementById("error");
-    document.getElementById("product-info").innerHTML = "";
+// const errorMessage = () => {
+//     const searchText = document.getElementById("input").value;
+//     const errorMessageDiv = document.getElementById("error");
+//     document.getElementById("product-info").innerHTML = "";
 
-    errorMessageDiv.innerHTML = ` <div class="card m-auto p-5 bg-warning" style="width: 18rem">
-            <h5 class="card-title">Dear Sir/Ma'am,</h5>
-            <p class="card-text">
-              Your search --<b>${searchText}</b>-- did not match any of our set meal. Please enter a
-              correct name.
-            </p>
-          </div>;
-          `
-};
+//     errorMessageDiv.innerHTML = ` <div class="card m-auto p-5 bg-warning" style="width: 18rem">
+//             <h5 class="card-title">Dear Sir/Ma'am,</h5>
+//             <p class="card-text">
+//               Your search --<b>${searchText}</b>-- did not match any of our set meal. Please enter a
+//               correct name.
+//             </p>
+//           </div>;
+//           `
+// };
 
 
 
