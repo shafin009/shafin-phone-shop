8879f397831bace1dde973d97a991e07439bbb58
shafin009 <ghostboyshafin@example.com>
2022-03-01 15:12:43 +0600

    done

--- script.js
+++ script.js
@@ -4,46 +4,81 @@
 const searchPhone = () => {
     const input = document.getElementById('input');
     const searchText = input.value;
-
     input.value = '';
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-
     fetch(url)
         .then(res => res.json())
-        .then(data => displayResult(data.data))
+        .then(data => displayResult(data.data.slice(0, 20)))
+    document.getElementById('error').innerText = ''
 
 }
 
-const displayResult = phones => {
+const displayResult = (phones) => {
 
-    const searchResult=document.getElementById('search-result');
+    const searchResult = document.getElementById('search-result')
+    searchResult.innerHTML = '';
 
-    phones.forEach(data => {
 
-        console.log(data)
+    phones.forEach(phone => {
 
-        const div=document.createElement('div');
+        // console.log(data)
+
+        const div = document.createElement('div');
         div.classList.add('col')
-        div.innerHTML=`
+        div.innerHTML = `
+            
+            <div class="card h-100">
+                     <img src="${phone.image}" class="card-img-top" alt="...">
+                 <div class="card-body">
+                     <h5 class="card-title">Model-${phone.phone_name}</h5>
+                     <h5 class="card-title">Brand-${phone.brand}</h5>
+                     <button onclick="seeDetails('${phone.slug}')"type="button" class="btn btn-primary btn-sm">Details</button>
+                 </div>
+            </div>
         
-        <div class="card h-100">
-                 <img src="${data.image}" class="card-img-top" alt="...">
-             <div class="card-body">
-                 <h5 class="card-title">Model-${data.phone_name}</h5>
-                 <h5 class="card-title">Brand-${data.brand}</h5>
-                 <p class="card-text">Details-${data.slug}</p>
-             </div>
-        </div>
-        
-        `;
+            
+            `;
 
         searchResult.appendChild(div);
 
-
-        
     });
+
+
+
+}
+
+const seeDetails = (id) => {
+
+    const url2 = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url2)
+        .then(res => res.json())
+        .then(data => information(data.data))
+
+
+}
+const information = (details) => {
+
+
+    const mobileDetails = document.getElementById('mobile-details')
+    mobileDetails.innerHTML = '';
+    const div = document.createElement('div');
+    div.classList.add('col')
+    div.innerHTML = `
     
+    <div class="card p-3 shadow-lg">
+        <img src="${details.image}" class="card-img-top img-fluid w-25 mx-auto" alt="...">
+        <div class="card-body">
+            <p class="card-title">Model-${details.name}</p>
+            <p9 class="card-title">Brand-${details.brand}</p>
+            
+        </div>
+    </div>
+    
+    
+    `
+    mobileDetails.appendChild(div);
+
 
 
 }
@@ -59,22 +94,3 @@ const displayResult = phones => {
 
 
 
-
-    // const loadPhone = () => {
-    //     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    //         .then(response => response.json())
-    //         .then(data => console.log(data.data));
-
-
-    // }
-    // loadPhone()
-
-    // const displayPhone = phone => {
-    //     const phone1Div = document.getElementById('phone1');
-    //     phone1Div.innerHTML = `
-    //         <h3 class="text-center text-uppercase text-light">Cases Sheet</h3>
-    //         <h3 class='text-light'>${phone.brand}</h3>
-    //         <h4 class='text-light'>Active Cases:</h4>
-    //         <h4 class='text-light'>Today Cases: </h4>   
-    //         <h4 class='text-light'>Active Per 1M:</h4>
-    //         `
